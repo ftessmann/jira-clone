7c017d807483879e6f01ce5fe6ec036f7c04557a
ftessmann <fernando.tessmann@gmail.com>
2024-10-25 16:41:48 -0300

    feat: sign in/up pages

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,9 @@
+"use client";
+
+import { SignInCard } from "@/features/auth/components/sign-in-card";
+
 const SignInPage = () => {
-    return (
-        <div>
-            Sign In page
-        </div>
-    );
+    return <SignInCard />
 };
 
 export default SignInPage;
