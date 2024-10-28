ea51a8deec17cdcb7d76964dfb5c22b3efafe1f6
ftessmann <fernando.tessmann@gmail.com>
2024-10-28 16:44:56 -0300

    refactor: changed const name

--- src/features/auth/schemas.ts
+++ src/features/auth/schemas.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const signUpSchema = z.object({
+export const registerSchema = z.object({
     name: z.string().trim().min(1, "Required"),
     email: z.string().email(),
     password: z.string().min(8, "Minimum 8 characters")
