007a43dd0ba58e838f45e2d5b30dc896a2293079
ftessmann <fernando.tessmann@gmail.com>
2024-10-31 09:54:46 -0300

    feat: logout function on root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { useRouter } from "next/navigation";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+	const router = useRouter();
+	const { data, isLoading } = useCurrent();
+	const { mutate } = useLogout();
+
+	useEffect(() => {
+		if (!data && !isLoading) {
+			router.push("/sign-in");
+		}
+	}, [data, isLoading, router]);
+
 	return (
     	<div className="flex gap-6">
-			
+			<Button onClick={() => mutate()}>
+				Logout
+			</Button>
     	</div>
   	);
 }
