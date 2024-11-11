7e73a39252ce75250e0b757f1355f421d3eaa8dd
ftessmann <fernando.tessmann@gmail.com>
2024-11-11 13:48:19 -0300

    feat: added image upload route/schema

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,12 +1,18 @@
 import { Hono } from "hono";
 
+import { ID } from "node-appwrite";
+
 import { zValidator } from "@hono/zod-validator";
 
 import { sessionMiddleware } from "@/lib/session-middleware";
 
 import { createWorkspaceSchema } from "../schemas";
-import { DATABASE_ID, WORKSPACES_ID } from "@/config";
-import { ID } from "node-appwrite";
+import { 
+    DATABASE_ID, 
+    IMAGES_BUCKET_ID, 
+    WORKSPACES_ID 
+} from "@/config";
+
 
 const app = new Hono()
     .post(
@@ -15,10 +21,28 @@ const app = new Hono()
         sessionMiddleware,
         async (c) => {
             const databases = c.get("databases");
+            const storage = c.get("storage");
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const user = c.get("user");
 
-            const { name } = c.req.valid("json");
+            const { name, image } = c.req.valid("json");
+
+            let uploadedImageUrl: string | undefined;
+
+            if (image instanceof File) {
+                const file = await storage.createFile(
+                    IMAGES_BUCKET_ID,
+                    ID.unique(),
+                    image,
+                );
+
+                const arrayBuffer = await storage.getFilePreview(
+                    IMAGES_BUCKET_ID,
+                    file.$id
+                );
+
+                uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+            };
 
             const workspace = await databases.createDocument(
                 DATABASE_ID,
@@ -26,7 +50,8 @@ const app = new Hono()
                 ID.unique(),
                 {
                     name,
-                    userId: user.$id
+                    userId: user.$id,
+                    imageUrl: uploadedImageUrl,
                 },
             );
 
