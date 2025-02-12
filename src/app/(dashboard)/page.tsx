510e3a9c76be46a897fb1dba428d03233a340503
ftessmann <fernando.tessmann@gmail.com>
2025-02-12 17:34:50 -0300

    feat: new redirect

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,18 +1,19 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/actions";
-import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
+import { getWorkspaces } from "@/features/workspaces/actions";
 
 export default async function Home() {
 	const user = await getCurrent();
+	
+	if (!user) redirect("/sign-in");
 
-	if (!user) {
-		redirect("/sign-in")
+	const workspaces = await getWorkspaces();
+
+	if (workspaces.total === 0) {
+		redirect("/workspaces/create")
+	} else {
+		redirect(`/workspaces/${workspaces.documents[0].$id}`)
 	}
 
-	return (
-    	<div className="p-4 bg-neutral-400 h-full w-full">
-			<CreateWorkspaceForm />
-    	</div>
-  	);
 };
