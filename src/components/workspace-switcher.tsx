d83997b63f019fd325ae12ccf37250368ec12cff
ftessmann <fernando.tessmann@gmail.com>
2025-02-13 15:54:18 -0300

    feat: added open hook

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+import { useCreateWorkspaceModal } from "@/features/workspaces/hooks/use-create-workspace-modal";
 
 import {
     Select,
@@ -12,12 +14,12 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
-import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
 
 export const WorkspaceSwitcher = () => {
     const workspaceId = useWorkspaceId();
     const router = useRouter();
     const { data: workspaces } = useGetWorkspaces();
+    const { open } = useCreateWorkspaceModal()
 
     const onSelect = (id: string) => {
         router.push(`/workspaces/${id}`)
@@ -27,7 +29,7 @@ export const WorkspaceSwitcher = () => {
         <div className="flex flex-col gap-y-2">
             <div className="flex items-center justify-between">
                 <p className="text-xs uppercase text-neutral-500">Workspaces</p>
-                <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"/>
+                <RiAddCircleFill onClick={open} className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"/>
             </div>
             <Select onValueChange={onSelect} value={workspaceId}>
                 <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
